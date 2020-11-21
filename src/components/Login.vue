f71902819133c808eb5852ebe0528a66230ad3b4
Kristen Surva <kristensurva@gmail.com>
2020-11-21 19:27:01 +0200

    Login nupp Route'b Main-Page'le nüüd

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -15,7 +15,7 @@
       </div>
       <div>
         <h3>Please Log In</h3>
-        <form action="index.html" method="post">
+        <form>
           <div>
             <input type="text" name="email" placeholder="Email">
           </div>
@@ -23,7 +23,7 @@
             <input type="password" name="password" placeholder="Password">
           </div>
           <div>
-            <button class="button" type="submit" name="login">Log In</button>
+            <button class="button" type="submit" name="login" @click="$router.push('main-page')">Log In</button>
           </div>
           <div>
             <small>
@@ -39,42 +39,152 @@
 
 <script>
 export default {
-name: "Login"
+name: "Login.vue"
 }
 </script>
 
 <style scoped>
-  body {
-    background-color: #0277bd;
-    color: #263238;
-  }
 
-  a {
-    color: #40c4ff;
-  }
+* {
+  font-family: 'Roboto Slab', serif;
+  outline: none;
+}
 
-  .login-page {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
 
-  #login-container {
-    background-image: url(/images/logo.png);
-    background-size: 20% auto;
-    background-repeat: no-repeat;
-    background-position: 15px 15px;
-    min-width: 25%;
-    padding: 45px 45px;
-    background-color: #ffffff;
-    box-shadow: 0 0 15px rgba(38, 50, 56, 0.33);
-    text-align: center;
-  }
+body {
+  background-color: #0277bd;
+  color: #263238;
+}
 
-  #login-container form div > input {
-    padding: 8px 16px;
-    margin: 4px 0;
-  }
+a {
+  color: #40c4ff;
+}
+
+.login-page {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#login-container {
+  background-image: url(/images/logo.png);
+  background-size: 20% auto;
+  background-repeat: no-repeat;
+  background-position: 15px 15px;
+  min-width: 25%;
+  padding: 45px 45px;
+  background-color: #ffffff;
+  box-shadow: 0 0 15px rgba(38, 50, 56, 0.33);
+  text-align: center;
+}
+
+#login-container form div > input {
+  padding: 8px 16px;
+  margin: 4px 0;
+}
+
+button {
+  padding: 8px 16px;
+  margin: 4px 0;
+  color: #ffffff;
+  background-color: #01579b;
+  border: none;
+  border-radius: 4px;
+}
+
+button:hover {
+  box-shadow: 0 0 5px rgba(38, 50, 56, 0.7);
+  cursor: pointer;
+}
+
+header {
+  position: fixed;
+  width: 100%;
+  top: 0;
+  z-index: 1;
+}
+
+header:hover {
+  box-shadow: 0 -20px 30px #4d4d4d;
+}
+
+nav {
+  display: flex;
+  background-color: #ffffff;
+  align-items: center;
+}
+
+nav div {
+  height: 30px;
+  flex-grow: 4;
+  padding: 10px;
+}
+
+nav div img {
+  height: 100%;
+  width: 30px;
+  margin-left: 15px;
+  border-radius: 100%;
+  object-fit: cover;
+  object-position: top center;
+}
+
+nav div.search-container > input {
+  box-sizing: border-box;
+  height: 30px;
+  width: 80%;
+  margin: 0;
+  padding: 5px;
+  border: 1px solid #e0e0e0;
+}
+
+nav div.search-container > button {
+  height: 30px;
+  width: 20%;
+  margin: 0;
+  padding: 5px;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.post .post-author .post-author-info img {
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  object-fit: cover;
+  object-position: top;
+  margin: 5px;
+}
+
+.post .post-author .post-author-info small {
+  position: absolute;
+  top: 10px;
+  left: 35px;
+}
+
+.post .post-author .post-author-info + small {
+  float: right;
+  color: grey;
+  padding: 10px;
+}
+
+.post .post-image img, video {
+  width: 100%;
+  min-height: 150px;
+  max-height: 350px;
+  object-fit: cover;
+  object-position: top center;
+}
+
+.post .post-title h3 {
+  display: inline;
+}
 </style>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -12,11 +12,6 @@ export default {
 
   components: {
     Login
-  },
-  methods: {
-    loginButtonPressed: function() {
-
-    }
   }
 }
 </script>
