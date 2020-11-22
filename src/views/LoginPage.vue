ee1813d1187d99d4a60cd9a76e337f676a30b5d8
Kristen Surva <kristensurva@gmail.com>
2020-11-22 21:14:23 +0200

    Login page valmis, stiil lisatud App.vue'sse.

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,21 +1,71 @@
 <template>
-  <div class="login-page">
-    <Login/> <!--Komponendi sisse toomine-->
-  </div>
+    <section class="login-page">
+      <section id="login-container">
+        <div>
+          <h1>Welcome to postIt!</h1>
+        </div>
+        <div>
+          <h4>
+            <a href="#">Create an Account</a>
+          </h4>
+        </div>
+        <div>
+          <h4>Or</h4>
+        </div>
+        <div>
+          <h3>Please Log In</h3>
+          <form>
+            <div>
+              <input type="text" name="email" placeholder="Email">
+            </div>
+            <div>
+              <input type="password" name="password" placeholder="Password">
+            </div>
+            <div>
+              <button class="button" type="submit" name="login" @click="$router.push('main-page')">Log In</button>
+            </div>
+            <div>
+              <small>
+                <a href="#">Forgot password?</a>
+              </small>
+            </div>
+          </form>
+        </div>
+      </section>
+    </section>
 </template>
 
 <script>
-import Login from "@/components/Login";
 
 export default {
   name: "LoginPage.vue",
-
-  components: {
-    Login
-  }
 }
 </script>
 
 <style scoped>
 
+.login-page {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#login-container {
+  background-image: url(logo.png);
+  background-size: 20% auto;
+  background-repeat: no-repeat;
+  background-position: 15px 15px;
+  min-width: 25%;
+  padding: 45px 45px;
+  background-color: #ffffff;
+  box-shadow: 0 0 15px rgba(38, 50, 56, 0.33);
+  text-align: center;
+}
+
+#login-container form div > input {
+  padding: 8px 16px;
+  margin: 4px 0;
+}
 </style>
